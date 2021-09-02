9de9997910aa2d2316c178b3e32d95ae46b1331b
Zac Messsinger <zmessinger@quantummetric.com>
2021-09-01 21:22:16 -0700

    installed bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import * as React from "react"
+import * as React from "react";
+import "./master.scss"
 
 const IndexPage = () => {
     return (
-        <div>
+        <div className="container">
             <h1>Hello World</h1>
         </div>
     )
