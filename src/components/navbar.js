8f2cf88c48d228f20e6fb055a92053e1690c207a
Zac Messsinger <zmessinger@quantummetric.com>
2021-09-13 21:19:11 -0700

    fontawesome added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../utils/fontawesome';
 
 const Navbar = () => {
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <ul className="navbar-menu">
-                <li className="navbar-start">Projects</li>
+                <ul className="navbar-start">
+                    <li className="navbar-item">Projects</li>
+                    <li className="navbar-item">Blog</li>
+                </ul>
                 <li className="navbar-item">Home</li>
-                <li className="navbar-end">Blog</li>
+                <ul className="navbar-end">
+                    <li className="navbar-item">
+                        <FontAwesomeIcon size="lg" icon={['fab', 'linkedin']}/>
+                    </li>
+                    <li className="navbar-item">
+                        <FontAwesomeIcon size="lg" icon={['fab', 'github']} />
+                    </li>
+                </ul>
+                
             </ul>
         </nav>
     )
---
+++ src/utils/fontawesome.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMobile, faCashRegister, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab, faMobile, faCashRegister, faCodeBranch);
