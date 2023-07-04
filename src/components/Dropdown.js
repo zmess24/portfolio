a6cc546f7a58b59dc724a7480ae375bc3d0b623f
Zac Messinger <zdmessinger@gmail.com>
2023-07-03 17:56:39 -0700

    added hamburger

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,37 +1,39 @@
-import * as React from 'react';
-import Card from './Card';
+import * as React from "react";
+import Card from "./Card";
 
-const Dropdown = ({dropDownState, data, handleClick}) => {
-    let classNames = dropDownState ? "dropdown show-dropdown" : "dropdown hide-dropdown";
-    return (
-        <main className={classNames}>
-             <nav className="navbar" role="navigation" aria-label="secondary">
-                <header className="navbar-menu">
-                    <section className="navbar-item">
-                        <a href="/" onClick={handleClick} title="close">X</a>
-                    </section>
-                    <section className="navbar-item navbar-brand">
-                        {/* <StaticImage alt="Logo" src="../images/logo_2.png" /> */}
-                        <a href="/">ZDM</a>
-                    </section>
-                </header> 
-            </nav>
-            <section className="content">
-                <div id="title-section">
-                    {data.name && <h2>{data.name}</h2>}
-                </div>
-                <ul>
-                    {data.data.map(d => {
-                        return ( 
-                            <li key={d}>
-                                <Card data={d}/> 
-                            </li>
-                        )
-                    })}
-                </ul>
-            </section>
-        </main>
-    )
+const Dropdown = ({ dropDownState, data, handleClick }) => {
+	let classNames = dropDownState ? "dropdown show-dropdown" : "dropdown hide-dropdown";
+	return (
+		<main className={classNames}>
+			<nav className="navbar" role="navigation" aria-label="secondary">
+				<header className="navbar-menu">
+					<section className="navbar-item">
+						<a className="hamburger" href="/" onClick={handleClick} title="close">
+							<span></span>
+							<span></span>
+							<span></span>
+						</a>
+					</section>
+					<section className="navbar-item navbar-brand">
+						{/* <StaticImage alt="Logo" src="../images/logo_2.png" /> */}
+						<a href="/">ZDM</a>
+					</section>
+				</header>
+			</nav>
+			<section className="content">
+				<div id="title-section">{data.name && <h2>{data.name}</h2>}</div>
+				<ul>
+					{data.data.map((d) => {
+						return (
+							<li key={d}>
+								<Card data={d} />
+							</li>
+						);
+					})}
+				</ul>
+			</section>
+		</main>
+	);
 };
 
 export default Dropdown;
