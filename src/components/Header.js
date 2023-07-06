66f04c0c4baf4b0085aa7e63de123b8710cdadf6
Zac Messinger <zdmessinger@gmail.com>
2023-07-05 17:19:53 -0700

    updated styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,30 @@
-import * as React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { StaticImage } from 'gatsby-plugin-image';
 
-const Header = ({ dropDownState, handleClick}) => {
-    let classNames = dropDownState ? "alternate" : "";
+const Header = ({ dropDownState, handleClick }) => {
+	let classNames = dropDownState ? "alternate" : "";
 
-    return (
-        <nav className="navbar" role="navigation" aria-label="primary">
-            <header className="navbar-menu" id="primary-nav">
-                <section className="navbar-item">
-                    <a href="/" title="open" onClick={handleClick}>Projects</a>
-                    <a href="/" title="open" onClick={handleClick}>Blog</a>
-                    <a href="/">Contact</a>
-                </section>
-                <section className="navbar-item navbar-brand is-hidden-mobile">
-                    <a href="/" className={classNames}>ZDM</a>
-                </section> 
-            </header>
-        </nav>
-    )
+	return (
+		<nav className="navbar" role="navigation" aria-label="primary">
+			<header className="navbar-menu" id="primary-nav">
+				<section className="navbar-item">
+					<a href="/" title="open" onClick={handleClick}>
+						Projects
+					</a>
+					<a href="/" title="open" onClick={handleClick}>
+						Blog
+					</a>
+					<a href="/">Contact</a>
+				</section>
+				<section className="navbar-item navbar-brand is-hidden-mobile">
+					<a href="/" className={classNames}>
+						Zachary Messinger
+					</a>
+				</section>
+			</header>
+		</nav>
+	);
 };
 
 export default Header;
